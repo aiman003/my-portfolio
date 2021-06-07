cc21eb4634db9ca14a587cac58d875361f0d885b
aiman003 <fatimaaiman760@gmail.com>
2021-06-07 02:04:19 -0700

    first commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,46 @@
+// eslint-disable-next-line
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+
+    if(this.props.data){
+      var networks= this.props.data.social.map(function(network){
+        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+      })
+    }
+
+    return (
+      <footer>
+        <div className="row">
+          <div className="twelve columns">
+            <ul className="social-links">{networks}</ul>
+
+            <ul>
+              <li>MADE WITH LOVE BY AIMAN FATIMA </li>
+            </ul>
+
+            <ul className="copyright">
+              <li>&copy; 2020 AIMAN</li>
+              <li>
+                Design by{""}
+                <a
+                  
+                >
+                   AYMAN FATIMA
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div id="go-top">
+            <a className="smoothscroll" title="Back to Top" href="#home">
+              <i className="icon-up-open"></i>
+            </a>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
